6740d55e42bacd2437025718863204f0b5c1e341
Lucas Amorim Maia <143425324+Kinhazin@users.noreply.github.com>
2024-06-09 15:44:40 -0300

    Update comeco.java

--- src/PROJETOINTEGRADOR/comeco.java
+++ src/PROJETOINTEGRADOR/comeco.java
@@ -16,7 +16,7 @@ public class comeco {
         System.out.printf("\nSeja muito bem - vindo, %s", nome);
         System.out.println("\nAntes de darmos inicio ao Quiz vamos falar de como ele funciona, ja que nao se trata de um Quiz chato e normal, e sim de um interativo e divertido!");
         System.out.println("\n * REGRAS *\n");
-        System.out.println("* O jogador possui x numeros de vida, dependendo do modo de jogo!\n* A cada erro, vocu perde 1 vida, entao tome cuidado, pois ao chegar a 0 vidas sera game over.");
+        System.out.println("* O jogador possui x numeros de vida, dependendo do modo de jogo!\n* A cada erro, você perde 1 vida, entao tome cuidado, pois ao chegar a 0 vidas sera game over.");
         System.out.println("* A cada acerto, alem de passar para a proxima pergunta, voce ganha pontos e moedas.\n* Voce pode trocar suas moedas por #Dicas - #Vida Extra - #NextQuestion - #Eliminar Alternativa.");
         System.out.println("* E tem mais! A cada alternativa certa, voce libera uma curiosidade sobre o mundo da tecnologia!\n");
         System.out.println("* REGRAS *");
