0af124e147661cd737d4e5fc1b940409d3713c30
Lucas Amorim Maia <143425324+Kinhazin@users.noreply.github.com>
2024-06-09 19:30:40 -0300

    Melhoria visual das perguntas

--- src/PROJETOINTEGRADOR/Perguntas.java
+++ src/PROJETOINTEGRADOR/Perguntas.java
@@ -5,12 +5,13 @@ import static PROJETOINTEGRADOR.Loja.Loja;
 public class Perguntas {
 
     public static int[][] PrimeiraPegunta(int parametros[][]) {
-        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
+        int auxiliares[] = new int[3]; // 1Âº Para enviar qual pergunta Ã©, 2Âº Variavel de controle de laÃ§o, 3Âº para saber se a resposta estÃ¡ certa ou errada            
         char alternativa;
         auxiliares[0] = 1;
-
+        
+        System.out.println("\n\n* PERGUNTA 1 *");
         System.out.println("\nSeja muito bem-vindo. Estamos muito felizes em ter você por aqui! Para embarcarmos "
-                + "em uma jornada de aprendizado e curiosidades, \nvamos primeiro ver qual o seu entendimento quando falamos sobre IA.\n");
+                + "em uma jornada de aprendizado e curiosidades, \nvamos primeiro ver qual o seu entendimento quando falamos sobre IA.");
         do {           
             mododejogo.mostrandoPontuacao(parametros);
             alternativa = Enunciados.enunciadoPergunta1();
@@ -31,11 +32,12 @@ public class Perguntas {
     }
 
     public static int[][] SegundaPegunta(int parametros[][]) {
-        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
+        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é a, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada           
         char alternativa;
         auxiliares[0] = 2;
-
-        System.out.println("\nAgora que já sabemos o que é inteligência artificial e o que essa área busca, vamos ver quando surgiu um dos primeiros vestígios desse conceito.\n");
+        
+        System.out.println("\n\n* PERGUNTA 2 *");
+        System.out.println("\nAgora que já sabemos o que é inteligência artificial e o que essa área busca, vamos ver quando surgiu um dos primeiros vestígios desse conceito.");
         do {          
             mododejogo.mostrandoPontuacao(parametros);
             alternativa = Enunciados.enunciadoPergunta2();
@@ -56,12 +58,13 @@ public class Perguntas {
     }
 
     public static int[][] terceiraPegunta(int parametros[][]) {
-        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
+        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é a, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada              
         char alternativa;
         auxiliares[0] = 3;
 
-        System.out.println("Tendo em mente como surgiu a ideia da inteligência artificial, devemos falar do primeiro evento relacionado a IA,\n"
-                + "que com certeza foi um marco para o seu desenvolvimento, a famosa conferência:\n");
+        System.out.println("\n\n* PERGUNTA 3 *");
+        System.out.println("\nTendo em mente como surgiu a ideia da inteligência artificial, devemos falar do primeiro evento relacionado a IA,\n"
+                + "que com certeza foi um marco para o seu desenvolvimento, a famosa conferência:");
         do {         
             mododejogo.mostrandoPontuacao(parametros);
             alternativa = Enunciados.enunciadoPergunta3();
@@ -82,12 +85,13 @@ public class Perguntas {
     }
 
     public static int[][] quartaPegunta(int parametros[][]) {
-        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
+        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é a, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
         char alternativa;
         auxiliares[0] = 4;
 
-        System.out.println("A conferência de Dartmouth certamente foi de extrema importância para o desenvolvimento da IA, tanto que,\n"
-                + "pela primeira vez foi apresentado um software que utilizou-se da tecnologia.\n");
+        System.out.println("\n\n* PERGUNTA 4 *");
+        System.out.println("\nA conferência de Dartmouth certamente foi de extrema importÃ¢ncia para o desenvolvimento da IA, tanto que,\n"
+                + "pela primeira vez foi apresentado um software que utilizou-se da tecnologia.");
         do {           
             mododejogo.mostrandoPontuacao(parametros);
             alternativa = Enunciados.enunciadoPergunta4();
@@ -107,12 +111,14 @@ public class Perguntas {
         return parametros;
     }
 
+    
     public static int[][] quintaPegunta(int parametros[][]) {
-        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
+        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é a, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
         char alternativa;
         auxiliares[0] = 5;
-
-        System.out.println("\nAo passar dos anos foram realizadas pesquisas, e a especialização na IA foi se aprofundando cada vez mais, hoje já contamos com campos específicos para cada área de estudo.\n");
+        
+        System.out.println("\n\n* PERGUNTA 5 *");
+        System.out.println("\nAo passar dos anos foram realizadas pesquisas, e a especialização na IA foi se aprofundando cada vez mais, hoje já contamos com campos específicos para cada área de estudo.");
         do {   
             mododejogo.mostrandoPontuacao(parametros);
             alternativa = Enunciados.enunciadoPergunta5();
@@ -133,12 +139,13 @@ public class Perguntas {
     }
 
     public static int[][] sextaPegunta(int parametros[][]) {
-        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
+        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é a, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
         char alternativa;
         auxiliares[0] = 6;
 
+        System.out.println("\n\n* PERGUNTA 6 *");    
         System.out.println("\nComo apresentado, vimos que a inteligência artificial está mais inserida no nosso cotidiano do que pensamos. Seguindo essa linha de raciocínio,\n"
-                + "é capaz da inteligência artificial estar presente na educação? A resposta é sim, é possível!\n");
+                + "é capaz da inteligência artificial estar presente na educação? A resposta é sim, é possível!");
         do {
             mododejogo.mostrandoPontuacao(parametros);
             alternativa = Enunciados.enunciadoPergunta6();
@@ -159,12 +166,13 @@ public class Perguntas {
     }
 
     public static int[][] setimaPegunta(int parametros[][]) {
-        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
+        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é a, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada          
         char alternativa;
         auxiliares[0] = 7;
 
-        System.out.println("\nContudo, para a inteligência artificial estar tão inserida na educação nos dias de hoje, houve um ponto de partida. Um dos primeiros softwares a utilizar IA,"
-                + "resolvendo problemas matemáticos descritos em palavras e fornecendo um retorno avaliativo com base no desempenho do aluno.\n");
+        System.out.println("\n\n* PERGUNTA 7 *");
+        System.out.println("\nContudo, para a inteligência artificial estar tão inserida na educação nos dias de hoje, houve um ponto de partida. Um dos primeiros softwares a utilizar IA,\n"
+                + "resolvendo problemas matemáticos descritos em palavras e fornecendo um retorno avaliativo com base no desempenho do aluno.");
         do {                   
             mododejogo.mostrandoPontuacao(parametros);
             alternativa = Enunciados.enunciadoPergunta7();
@@ -185,11 +193,12 @@ public class Perguntas {
     }
 
     public static int[][] oitavaPegunta(int parametros[][]) {
-        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada            
+        int auxiliares[] = new int[3]; // 1º Para enviar qual pergunta é a, 2º Variavel de controle de laço, 3º para saber se a resposta está certa ou errada          
         char alternativa;
         auxiliares[0] = 8;
 
-        System.out.println("\nNão se limitando apenas à educação, a inteligência artificial vem cada vez mais ganhando espaço na área da saúde, contribuindo com acompanhamentos personalizados e decisões médicas.\n");
+        System.out.println("\n\n* PERGUNTA 8 *");
+        System.out.println("\nNão se limitando apenas à educação, a inteligência artificial vem cada vez mais ganhando espaço na área da saúde, contribuindo com acompanhamentos personalizados e decisões médicas.");
         do {
             mododejogo.mostrandoPontuacao(parametros);
             alternativa = Enunciados.enunciadoPergunta8();
